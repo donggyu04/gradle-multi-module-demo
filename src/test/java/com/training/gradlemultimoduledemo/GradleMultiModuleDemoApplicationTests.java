270bc6141b2ba83838fa1aa80aae95176061a8fa
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-08 03:03:15 +0900

    Add skeleton

---
+++ src/test/java/com/training/gradlemultimoduledemo/GradleMultiModuleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.gradlemultimoduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleMultiModuleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
