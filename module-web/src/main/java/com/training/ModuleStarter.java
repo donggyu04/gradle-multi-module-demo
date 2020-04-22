d0cfd8cca76457bb2fdb82214ee145e2fbc59f27
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-22 12:02:10 +0900

    Fix build failure

---
+++ module-web/src/main/java/com/training/ModuleStarter.java
@@ -0,0 +1,11 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleStarter.class, args);
+    }
+}
